63a9e500d7634ba572422f9a29dbfa3c79560dfe
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-07-25 13:10:14 -0400

    flake update

--- home/common-pkg.nix
+++ home/common-pkg.nix
@@ -59,6 +59,7 @@
     nodejs # JavaScript runtime environment
     yarn # Fast, reliable, and secure dependency management
     pnpm # Fast, disk space efficient package manager
+    bun # Fast, disk space efficient package manager
 
     # Python ecosystem (core tools)
     uv # Fast Python package installer and resolver
