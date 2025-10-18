fd230d2a6173610f19291b1e41b719e62c42fa81
hackiri <128340174+Hackiri@users.noreply.github.com>
2025-10-18 18:28:41 -0400

    fix: update branch filtering pattern to match HEAD refs without whitespace requirement

--- home/programs/shells/zsh/fzf-git.nix
+++ home/programs/shells/zsh/fzf-git.nix
@@ -29,7 +29,7 @@ _: ''
   # Shows local and remote branches with commit history preview
   _gb() {
     is_in_git_repo || return
-    git branch -a --color=always | grep -v '/HEAD\s' | sort |
+    git branch -a --color=always | grep -v '/HEAD' | sort |
     fzf-down --ansi --multi --tac --preview-window right:70% \
       --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
     sed 's/^..//' | cut -d' ' -f1 |
