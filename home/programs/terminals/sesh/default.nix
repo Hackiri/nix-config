a1eda3058a9c0585fb611c827c182ec149763e9e
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-03-29 14:21:26 -0400

    feat: add sesh session manager module

---
+++ home/programs/terminals/sesh/default.nix
@@ -0,0 +1,18 @@
+{...}: {
+  programs.sesh = {
+    enable = true;
+    enableTmuxIntegration = true;
+    tmuxKey = "f";
+    icons = true;
+    settings = {
+      default_session = {
+        startup_command = "nvim";
+        preview_command = "eza --all --git --icons --color=always {}";
+      };
+      sort_order = ["config" "tmux" "zoxide"];
+      session = [
+        {name = "nix-config"; path = "~/nix-config";}
+      ];
+    };
+  };
+}
