db5b5767896faf0dd3553dbe88eef3b04bc89c09
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-04-26 11:01:50 -0400

    refactor(nix): remove experimental and optimization settings

--- modules/system/shared/nix-settings.nix
+++ modules/system/shared/nix-settings.nix
@@ -6,11 +6,6 @@
 }: let
   commonSettings = {
     download-buffer-size = 268435456;
-    auto-optimise-store = false; # Disabled: race condition (NixOS/nix#7273); run `nix-store --optimise` manually
-    experimental-features = "nix-command flakes";
-    extra-experimental-features = "parallel-eval";
-    eval-cores = 0;
-    lazy-trees = true;
   };
   toNixConf = lib.concatStringsSep "\n" (
     lib.mapAttrsToList (
