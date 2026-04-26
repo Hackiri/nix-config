1624a57940a83f1833da7278ecc65eb6c7862998
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-04-26 01:32:32 -0400

    feat(nix): add experimental and performance settings to nix-settings.nix
    - experimental-features = nix-command flakes
    - extra-experimental-features = parallel-eval
    - eval-cores = 0
    - lazy-trees = true

--- modules/system/shared/nix-settings.nix
+++ modules/system/shared/nix-settings.nix
@@ -7,6 +7,10 @@
   commonSettings = {
     download-buffer-size = 268435456;
     auto-optimise-store = false; # Disabled: race condition (NixOS/nix#7273); run `nix-store --optimise` manually
+    experimental-features = "nix-command flakes";
+    extra-experimental-features = "parallel-eval";
+    eval-cores = 0;
+    lazy-trees = true;
   };
   toNixConf = lib.concatStringsSep "\n" (lib.mapAttrsToList (
       k: v: "${k} = ${
