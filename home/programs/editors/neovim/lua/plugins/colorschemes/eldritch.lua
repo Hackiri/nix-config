6af9ac318dca93676630cca2c1e9fb8b1c9d2b82
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-04-29 23:08:44 -0400

    Tidy Eldritch overrides

--- home/programs/editors/neovim/lua/plugins/colorschemes/eldritch.lua
+++ home/programs/editors/neovim/lua/plugins/colorschemes/eldritch.lua
@@ -1,9 +1,7 @@
 -- https://github.com/eldritch-theme/eldritch.nvim
 --
 
--- Require the colors.lua module and access the colors directly without
--- additional file reads
-local colors = require("config.colors")
+local palette = require("config.colors")
 
 return {
   "eldritch-theme/eldritch.nvim",
@@ -23,202 +21,125 @@ return {
       sidebars = "dark",
       floats = "dark",
     },
-    -- Overriding colors globally using a definitions table
-    on_colors = function(global_colors)
-      -- Define all color overrides in a single table
+    on_colors = function(colors)
       local color_definitions = {
-        -- https://github.com/eldritch-theme/eldritch.nvim/blob/master/lua/eldritch/colors.lua
-        bg = colors["color10"],
-        fg = colors["color14"],
-        selection = colors["color16"],
-        comment = colors["color09"],
-        red = colors["color08"], -- default #f16c75
-        orange = colors["color06"], -- default #f7c67f
-        yellow = colors["color05"], -- default #f1fc79
-        green = colors["color02"],
-        purple = colors["color04"], -- default #a48cf2
-        cyan = colors["color03"],
-        pink = colors["color01"], -- default #f265b5
-        bright_red = colors["color08"],
-        bright_green = colors["color02"],
-        bright_yellow = colors["color05"],
-        bright_blue = colors["color04"],
-        bright_magenta = colors["color01"],
-        bright_cyan = colors["color03"],
-        bright_white = colors["color14"],
-        menu = colors["color10"],
-        visual = colors["color16"],
-        gutter_fg = colors["color16"],
-        nontext = colors["color16"],
-        white = colors["color14"],
-        black = colors["color10"],
+        bg = palette.color10,
+        fg = palette.color14,
+        comment = palette.color09,
+        selection = palette.color16,
+        visual = palette.color16,
+        bg_visual = palette.color16,
+
+        red = palette.color11,
+        orange = palette.color08,
+        yellow = palette.color12,
+        green = palette.color02,
+        purple = palette.color04,
+        cyan = palette.color03,
+        pink = palette.color01,
+
+        bright_red = palette.color11,
+        bright_green = palette.color02,
+        bright_yellow = palette.color12,
+        bright_blue = palette.color04,
+        bright_magenta = palette.color01,
+        bright_cyan = palette.color03,
+        bright_white = palette.color14,
+
+        white = palette.color14,
+        black = palette.color10,
+        menu = palette.color10,
+        bg_dark = palette.color17,
+        bg_highlight = palette.color17,
+        terminal_black = palette.color13,
+        fg_dark = palette.color09,
+        fg_gutter = palette.color13,
+        fg_gutter_light = palette.color09,
+        dark3 = palette.color13,
+        dark5 = palette.color13,
+        dark_cyan = palette.color03,
+        dark_yellow = palette.color08,
+        dark_green = palette.color02,
+        magenta = palette.color01,
+        magenta2 = palette.color01,
+        magenta3 = palette.color21,
+
         git = {
-          change = colors["color03"],
-          add = colors["color02"],
-          delete = colors["color11"],
+          change = palette.color03,
+          add = palette.color02,
+          delete = palette.color11,
         },
         gitSigns = {
-          change = colors["color03"],
-          add = colors["color02"],
-          delete = colors["color11"],
+          change = palette.color03,
+          add = palette.color02,
+          delete = palette.color11,
         },
-        bg_dark = colors["color13"],
-        -- Lualine line across
-        bg_highlight = colors["color17"],
-        terminal_black = colors["color13"],
-        fg_dark = colors["color14"],
-        fg_gutter = colors["color13"],
-        dark3 = colors["color13"],
-        dark5 = colors["color13"],
-        bg_visual = colors["color16"],
-        dark_cyan = colors["color03"],
-        magenta = colors["color01"],
-        magenta2 = colors["color01"],
-        magenta3 = colors["color01"],
-        dark_yellow = colors["color05"],
-        dark_green = colors["color02"],
       }
 
-      -- Apply each color definition to global_colors
       for key, value in pairs(color_definitions) do
-        global_colors[key] = value
+        colors[key] = value
       end
     end,
 
-    -- This function is found in the documentation
-    on_highlights = function(highlights)
+    on_highlights = function(highlights, colors)
       local highlight_definitions = {
-        -- nvim-spectre or grug-far.nvim highlight colors
-        DiffChange = { bg = colors["color03"], fg = "black" },
-        DiffDelete = { bg = colors["color11"], fg = "black" },
-        DiffAdd = { bg = colors["color02"], fg = "black" },
-        FzfLuaResultsDiffDelete = { bg = colors["color01"], fg = "black" },
+        DiffAdd = { bg = colors.green, fg = colors.black },
+        DiffChange = { bg = colors.cyan, fg = colors.black },
+        DiffDelete = { bg = colors.red, fg = colors.black },
+        FzfLuaResultsDiffDelete = { bg = colors.red, fg = colors.black },
 
-        -- horizontal line that goes across where cursor is
-        CursorLine = { bg = colors["color13"] },
+        CursorLine = { bg = colors.terminal_black },
+        ColorColumn = { bg = colors.terminal_black },
+        Cursor = { bg = palette.color24 },
+        lCursor = { bg = palette.color24 },
+        CursorIM = { bg = palette.color24 },
+        Visual = { bg = colors.bg_visual, fg = colors.black },
 
-        -- Set cursor color, these will be called by the "guicursor" option in
-        -- the options.lua file, which will be used by neovide
-        Cursor = { bg = colors["color24"] },
-        lCursor = { bg = colors["color24"] },
-        CursorIM = { bg = colors["color24"] },
-
-        -- I do the line below to change the color of bold text
-        ["@markup.strong"] = { fg = colors["color24"], bold = true },
-
-        -- Inline code in markdown
-        ["@markup.raw.markdown_inline"] = { fg = colors["color02"] },
-        -- Background color of markdown folds
-        -- Folded = { bg = colors["color04"] },
-        -- Set this to NONE when handling transparency in the terminal and not
-        -- through yabai
+        ["@markup.strong"] = { fg = palette.color24, bold = true },
+        ["@markup.raw.markdown_inline"] = { fg = colors.green },
         Folded = { bg = "NONE" },
+        RenderMarkdownCode = { bg = palette.color07 },
+        RenderMarkdownQuote = { fg = colors.yellow },
 
-        -- Change the spell underline color
-        SpellBad = { sp = colors["color11"], undercurl = true, bold = true, italic = true },
-        SpellCap = { sp = colors["color12"], undercurl = true, bold = true, italic = true },
-        SpellLocal = { sp = colors["color12"], undercurl = true, bold = true, italic = true },
-        SpellRare = { sp = colors["color04"], undercurl = true, bold = true, italic = true },
+        SpellBad = { sp = colors.red, undercurl = true, bold = true, italic = true },
+        SpellCap = { sp = colors.yellow, undercurl = true, bold = true, italic = true },
+        SpellLocal = { sp = colors.yellow, undercurl = true, bold = true, italic = true },
+        SpellRare = { sp = colors.purple, undercurl = true, bold = true, italic = true },
 
-        MiniDiffSignAdd = { fg = colors["color05"], bold = true },
-        MiniDiffSignChange = { fg = colors["color02"], bold = true },
+        MiniDiffSignAdd = { fg = colors.green, bold = true },
+        MiniDiffSignChange = { fg = colors.cyan, bold = true },
 
-        -- Codeblocks for the render-markdown plugin
-        RenderMarkdownCode = { bg = colors["color07"] },
+        DiagnosticError = { fg = colors.red },
+        DiagnosticWarn = { fg = colors.yellow },
+        DiagnosticInfo = { fg = colors.cyan },
+        DiagnosticHint = { fg = colors.green },
+        DiagnosticOk = { fg = colors.green },
 
-        -- This is the plugin that shows you where you are at the top
-        TreesitterContext = { sp = colors["color10"] },
-        MiniFilesNormal = { sp = colors["color10"] },
-        MiniFilesBorder = { sp = colors["color10"] },
-        MiniFilesTitle = { sp = colors["color10"] },
-        MiniFilesTitleFocused = { sp = colors["color10"] },
+        PreProc = { fg = palette.color06 },
+        ["@operator"] = { fg = colors.green },
 
-        -- Set LazyGit transparent
-        -- NormalFloat = { bg = colors["color10"] },
-        NormalFloat = { bg = "NONE" },
+        KubectlHeader = { fg = colors.purple },
+        KubectlWarning = { fg = colors.yellow },
+        KubectlError = { fg = colors.red },
+        KubectlInfo = { fg = colors.cyan },
+        KubectlDebug = { fg = colors.purple },
+        KubectlSuccess = { fg = colors.green },
+        KubectlPending = { fg = colors.yellow },
+        KubectlDeprecated = { fg = colors.comment },
+        KubectlExperimental = { fg = colors.pink },
+        KubectlNote = { fg = colors.cyan },
+        KubectlGray = { fg = colors.fg_gutter_light },
 
-        FloatBorder = { bg = colors["color10"] },
-        FloatTitle = { bg = colors["color10"] },
-        NotifyBackground = { bg = colors["color10"] },
-        NeoTreeNormalNC = { bg = colors["color10"] },
-        NeoTreeNormal = { bg = colors["color10"] },
-        NvimTreeWinSeparator = { fg = colors["color10"], bg = colors["color10"] },
-        NvimTreeNormalNC = { bg = colors["color10"] },
-        NvimTreeNormal = { bg = colors["color10"] },
-        TroubleNormal = { bg = colors["color10"] },
-
-        NoiceCmdlinePopupBorder = { fg = colors["color10"] },
-        NoiceCmdlinePopupTitle = { fg = colors["color10"] },
-        NoiceCmdlinePopupBorderFilter = { fg = colors["color10"] },
-        NoiceCmdlineIconFilter = { fg = colors["color10"] },
-        NoiceCmdlinePopupTitleFilter = { fg = colors["color10"] },
-        NoiceCmdlineIcon = { fg = colors["color10"] },
-        NoiceCmdlineIconCmdline = { fg = colors["color03"] },
-        NoiceCmdlinePopupBorderCmdline = { fg = colors["color02"] },
-        NoiceCmdlinePopupTitleCmdline = { fg = colors["color03"] },
-        NoiceCmdlineIconSearch = { fg = colors["color04"] },
-        NoiceCmdlinePopupBorderSearch = { fg = colors["color03"] },
-        NoiceCmdlinePopupTitleSearch = { fg = colors["color04"] },
-        NoiceCmdlineIconLua = { fg = colors["color05"] },
-        NoiceCmdlinePopupBorderLua = { fg = colors["color01"] },
-        NoiceCmdlinePopupTitleLua = { fg = colors["color05"] },
-        NoiceCmdlineIconHelp = { fg = colors["color12"] },
-        NoiceCmdlinePopupBorderHelp = { fg = colors["color08"] },
-        NoiceCmdlinePopupTitleHelp = { fg = colors["color12"] },
-        NoiceCmdlineIconInput = { fg = colors["color10"] },
-        NoiceCmdlinePopupBorderInput = { fg = colors["color10"] },
-        NoiceCmdlinePopupTitleInput = { fg = colors["color10"] },
-        NoiceCmdlineIconCalculator = { fg = colors["color10"] },
-        NoiceCmdlinePopupBorderCalculator = { fg = colors["color10"] },
-        NoiceCmdlinePopupTitleCalculator = { fg = colors["color10"] },
-        NoiceCompletionItemKindDefault = { fg = colors["color10"] },
-
-        NoiceMini = { bg = colors["color10"] },
-        -- Winbar is liked to the StatusLine color, so to set winbar
-        -- transparent, I set the bg to NONE
-        StatusLine = { bg = "NONE" },
-        -- StatusLine = { bg = colors["color10"] },
-
-        DiagnosticInfo = { fg = colors["color03"] },
-        DiagnosticHint = { fg = colors["color02"] },
-        DiagnosticWarn = { fg = colors["color12"] },
-        DiagnosticOk = { fg = colors["color04"] },
-        DiagnosticError = { fg = colors["color11"] },
-        RenderMarkdownQuote = { fg = colors["color12"] },
-
-        -- visual mode selection
-        Visual = { bg = colors["color16"], fg = colors["color10"] },
-        PreProc = { fg = colors["color06"] },
-        ["@operator"] = { fg = colors["color02"] },
-
-        KubectlHeader = { fg = colors["color04"] },
-        KubectlWarning = { fg = colors["color03"] },
-        KubectlError = { fg = colors["color01"] },
-        KubectlInfo = { fg = colors["color02"] },
-        KubectlDebug = { fg = colors["color05"] },
-        KubectlSuccess = { fg = colors["color02"] },
-        KubectlPending = { fg = colors["color03"] },
-        KubectlDeprecated = { fg = colors["color08"] },
-        KubectlExperimental = { fg = colors["color09"] },
-        KubectlNote = { fg = colors["color03"] },
-        KubectlGray = { fg = colors["color10"] },
-
-        -- Colorcolumn that helps me with markdown guidelines
-        ColorColumn = { bg = colors["color13"] },
-
-        FzfLuaFzfPrompt = { fg = colors["color04"], bg = colors["color10"] },
-        FzfLuaCursorLine = { fg = colors["color02"], bg = colors["color10"] },
-        FzfLuaTitle = { fg = colors["color03"], bg = colors["color10"] },
-        FzfLuaSearch = { fg = colors["color14"], bg = colors["color10"] },
-        FzfLuaBorder = { fg = colors["color02"], bg = colors["color10"] },
-        FzfLuaNormal = { fg = colors["color14"], bg = colors["color10"] },
-
-        FzfLuaNormal = { fg = colors["color14"], bg = colors["color10"] },
-        FzfLuaMultiSelection = { fg = colors["color02"], bg = colors["color10"] },
-        FzfLuaSelection = { fg = colors["color14"], bg = colors["color13"] },
+        FzfLuaFzfPrompt = { fg = colors.purple, bg = colors.bg },
+        FzfLuaCursorLine = { fg = colors.green, bg = colors.terminal_black },
+        FzfLuaTitle = { fg = colors.cyan, bg = colors.bg },
+        FzfLuaSearch = { fg = colors.fg, bg = colors.bg },
+        FzfLuaBorder = { fg = colors.green, bg = colors.bg },
+        FzfLuaNormal = { fg = colors.fg, bg = colors.bg },
+        FzfLuaMultiSelection = { fg = colors.green, bg = colors.bg },
+        FzfLuaSelection = { fg = colors.fg, bg = colors.terminal_black },
       }
 
-      -- Apply all highlight definitions at once
       for group, props in pairs(highlight_definitions) do
         highlights[group] = props
       end
