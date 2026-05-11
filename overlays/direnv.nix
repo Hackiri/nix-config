4f64e1aafb76865047e07a63eb966b62e34fdbe9
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-05-10 22:17:39 -0400

    chore: remove obsolete overlay fixes

--- overlays/direnv.nix
+++
@@ -1,6 +0,0 @@
-# Override direnv to skip tests (aliases test hangs in sandbox on macOS)
-_: _final: prev: {
-  direnv = prev.direnv.overrideAttrs (_: {
-    doCheck = false;
-  });
-}
--- overlays/nodejs.nix
+++
@@ -1,6 +0,0 @@
-# Override nodejs to skip tests (network tests fail in sandbox)
-_: _final: prev: {
-  nodejs = prev.nodejs.overrideAttrs (_: {
-    doCheck = false;
-  });
-}
