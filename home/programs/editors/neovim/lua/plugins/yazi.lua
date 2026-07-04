55ddd21539b9a5d987fe8c5a25d066b4a5841ff6
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-07-04 09:31:34 -0400

    fix: configure yazi realpath resolver

--- home/programs/editors/neovim/lua/plugins/yazi.lua
+++ home/programs/editors/neovim/lua/plugins/yazi.lua
@@ -28,7 +28,9 @@ return {
     keymaps = {
       show_help = "<f1>",
     },
-    -- Use Nix's GNU realpath instead of Homebrew's grealpath on macOS
-    resolve_relative_path_application = "realpath",
+    integrations = {
+      -- Use realpath from PATH instead of Homebrew coreutils' grealpath on macOS.
+      resolve_relative_path_application = "realpath",
+    },
   },
 }
