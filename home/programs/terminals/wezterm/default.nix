56b659485b5857ef151372debcb69865053d41b5
hackiri <128340174+Hackiri@users.noreply.github.com>
2026-07-11 12:05:47 -0400

    fix: keep Darwin editors on stable nixpkgs

--- home/programs/terminals/wezterm/default.nix
+++ home/programs/terminals/wezterm/default.nix
@@ -1,16 +1,6 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: let
-  unstablePkgs = import inputs.nixpkgs-unstable {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    config.allowUnfree = true;
-    config.allowDeprecatedx86_64Darwin = true;
-  };
-in {
+{pkgs, ...}: {
   config = {
-    home.packages = [unstablePkgs.wezterm];
+    home.packages = [pkgs.wezterm];
 
     xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
   };
